663340632afb127d3a190b61566e2b2cd8a69d43
su <susana.salvatierra@ikasle.egibide.org>
2018-09-24 23:52:50 +0200

    CARPETA DRIVERS + SQLITE

--- Desarrollo/src/controlador/SQLite.java
+++ Desarrollo/src/controlador/SQLite.java
@@ -2,6 +2,17 @@ package controlador;
 
 public class SQLite {
 	
+	public static void main(String[] args) {
+		
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("org.sqlite.JDBC");
+		}
+	}
+	
 	
 
 }
