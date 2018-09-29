86ddbbea96ebe21468bce2ada11cbec634a8bc3d
Unknown <tehenua@gmail.com>
2018-09-30 00:07:52 +0200

    Inserts

--- Desarrollo/src/controlador/GestorBBDD.java
+++ Desarrollo/src/controlador/GestorBBDD.java
@@ -1,8 +1,122 @@
 package controlador;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import modelo.Empleado;
+import modelo.Pelicula;
+import modelo.Sala;
+
 public class GestorBBDD {
 	
-	private static final String mysqlConector = "mysql";
-    private static final String db4oConector = "db4o";
+	private String baseDatos;
+	private String url;
+	private String user;
+	private String passw;
+	private Connection con;
+	
+	//Constructor SQLite
+	public GestorBBDD(String baseDatos) {
+		switch(baseDatos) {
+		case "postgre":
+			url = "jdbc:postgresql://localhost:5433/cines";
+		    user = "postgres";
+		    passw = "";
+		    try {
+		    	Class.forName("org.postgresql.Driver"); 
+				con = DriverManager.getConnection(url,user,passw);
+			} catch (SQLException e) {
+				javax.swing.JOptionPane.showMessageDialog(null ,"Ha ocurrido un problema \n"+e.getMessage());
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				javax.swing.JOptionPane.showMessageDialog(null ,"Ha ocurrido un problema \n"+e.getMessage());
+				e.printStackTrace();
+			}
+			break;
+		case "mysql":
+			
+			break;
+		case "sqlite":
+			
+			break;			
+		}
+	}
 
+	
+	public boolean guardarSala(Sala sala) {
+		try {
+			String query = "INSERT INTO "+'"'+"Sala"+'"'+"(numero,aforo,dimensiones_pantalla,"
+					+ "ano_inauguracion,discapacidad,alta) VALUES(?,?,?,?,?,?)";
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, sala.getNumero());
+			ps.setInt(2, sala.getAforo());
+			ps.setString(3, sala.getDimPantalla());
+			ps.setInt(4, sala.getAnoInauguracion());
+			ps.setBoolean(5, sala.isDiscapacidad());
+			ps.setBoolean(6, sala.isAlta());
+			ps.execute();
+			ps.close();
+			return true;
+		} catch (SQLException e) {
+			javax.swing.JOptionPane.showMessageDialog(null ,"Ha ocurrido un problema \n"+e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+		//"INSERT INTO sala (nombre,apellido,cargo,fechanacimiento,"
+		//+ "nacionalidad,fechacontratacion,fechafincontrato,alta VALUES(?,?,"
+		//+ "?,?,?,?,?,?))"
+	}
+	
+	public boolean guardarEmpleado(Empleado empleado) {
+		try {
+			String query = "INSERT INTO "+'"'+"Empleado"+'"'+" (nombre,apellido,cargo,fechanacimiento,"
+					+ "nacionalidad,fechacontratacion,fechafincontrato,alta) VALUES(?,?,"
+					+ "?,?,?,?,?,?)";
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setString(1, empleado.getNombre());
+			ps.setString(2, empleado.getApellido());		
+			ps.setString(3, empleado.getCargo().toString());
+			ps.setDate(4, empleado.getFechaNacimiento());
+			ps.setString(5, empleado.getNacionalidad());
+			ps.setDate(6, empleado.getFechaContratacion());
+			ps.setDate(7, empleado.getFechaFinContrato());
+			ps.setBoolean(8, empleado.isAlta());
+			ps.execute();
+			ps.close();
+			return true;
+		} catch (SQLException e) {
+			javax.swing.JOptionPane.showMessageDialog(null ,"Ha ocurrido un problema \n"+e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+	
+	public boolean guardarPelicula(Pelicula pelicula) {
+		try {
+			String query = "INSERT INTO "+'"'+"Pelicula"+'"'+" (titulo,ano_estreno,director,actor_principal,"
+					+ "actor_secundario,duracion,trailer,alta) VALUES(?,?,"
+					+ "?,?,?,?,?,?)";
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setString(1, pelicula.getTitulo());
+			ps.setInt(2, pelicula.getAnoEstreno());		
+			ps.setString(3, pelicula.getDirector());
+			ps.setString(4, pelicula.getActorPrincipal());
+			ps.setString(5, pelicula.getActorSecundario());
+			ps.setInt(6, pelicula.getDuracion());
+			ps.setString(7, pelicula.getTrailer());
+			ps.setBoolean(8, pelicula.isAlta());
+			ps.execute();
+			ps.close();
+			return true;
+		} catch (SQLException e) {
+			javax.swing.JOptionPane.showMessageDialog(null ,"Ha ocurrido un problema \n"+e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
 }
--- Desarrollo/src/controlador/PostgreSQL.java
+++ Desarrollo/src/controlador/PostgreSQL.java
@@ -1,29 +1,25 @@
 package controlador;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
+
+import javax.swing.JOptionPane;
+
+import modelo.*;
 public class PostgreSQL {
 	public static void main(String args[]) throws Exception {
-	    Class.forName("org.postgresql.jdbc3");
-	    String url = "jdbc:postgresql://localhost:5432/cines";
-	    String user = "postgres";
-	    String pass = "cines1510";
-	    Connection con = null;
-	    try{
-	    	con = DriverManager.getConnection(url,user,pass);
-	    	Statement stmt = con.createStatement();
-		    ResultSet rs = stmt.executeQuery("SELECT VESION()");
-		    while (rs.next()) {
-		    	System.out.println(rs.getString(1));
-			}
-	    }catch (SQLException ex) {
-	    	ex.printStackTrace();
+   
+	    
+	    GestorBBDD gb = new GestorBBDD("postgre");
+	    Sala sala = new Sala(0, 0, "alalal", 0, false);
+	    Pelicula p = new Pelicula("dfsf", 0, "dfhakshf", "sdfasdf", "dsffse", 0, "dfsfd");
+	    Empleado e = new Empleado("fjkdjf", "dfdf", Cargo.camarero, new java.sql.Date(1985, 6, 10), "dfsdf", new java.sql.Date(1985, 6, 10), new java.sql.Date(1985, 6, 10));
+	    boolean correcto = gb.guardarEmpleado(e);
+	    if(correcto) {
+	    	JOptionPane.showMessageDialog(null, "Correcto", null, 0);
+	    }else {
+	    	JOptionPane.showMessageDialog(null, "Error", null, 0);
 	    }
-	    		
-	    con.close();
-	  }
+	}
+	
+
 }
