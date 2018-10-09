8afdfdb54d514e1f8951f0ee32215f9ac547978a
Sheilapsm <murgia85@gmail.com>
2018-10-09 23:21:25 +0200

    alta salas

--- Desarrollo/src/vista/VAltaSalas.java
+++ Desarrollo/src/vista/VAltaSalas.java
@@ -1,34 +1,27 @@
 package vista;
 
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JComboBox;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import com.toedter.calendar.JDateChooser;
-import java.awt.Rectangle;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.Frame;
+import java.awt.Rectangle;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 import com.toedter.calendar.JYearChooser;
 import javax.swing.JSpinner;
 
-public class VAltaSalas extends JPanel {
-	private JTextField txtDiscapacidad;
-	private JTextField txtDimPantalla;
-	private JTextField txtAnoInaug;
-	private JTextField txtResponsable;
+public class VAltaSalas extends JFrame {
 
+	private JPanel contentPane;
 
-	
 	/**
 	 * Launch the application.
 	 */
@@ -36,104 +29,115 @@ public class VAltaSalas extends JPanel {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					VAltaSalas panel = new VAltaSalas();
-					panel.setVisible(true);
+					VAltaSalas frame = new VAltaSalas();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-	
+
 	/**
 	 * Create the frame.
 	 */
 	public VAltaSalas() {
+		
 		setBounds(new Rectangle(0, 0, 800, 800));
-		setLayout(null);
+		
+		// Poner icono ventana
+		setIconImage(new ImageIcon(getClass().getResource("../imagenes/icono.png")).getImage());
+		
+		setExtendedState(Frame.NORMAL);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 800, 800);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		getContentPane().setBackground(new Color(233, 69, 75));
 		
 		JLabel cine = new JLabel("Cine");
-		cine.setBounds(100, 150, 300, 31);
-		add(cine);
 		cine.setForeground(Color.WHITE);
 		cine.setFont(new Font("Tahoma", Font.BOLD, 25));
+		cine.setBounds(100, 140, 300, 31);
+		contentPane.add(cine);
 		
 		JLabel numSala = new JLabel("N\u00FAmero Sala:");
-		numSala.setBounds(50, 266, 300, 31);
-		add(numSala);
-		numSala.setForeground(new Color(255, 255, 255));
+		numSala.setForeground(Color.WHITE);
 		numSala.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		numSala.setBounds(50, 256, 300, 31);
+		contentPane.add(numSala);
 		
 		JLabel aforo = new JLabel("Aforo:");
-		aforo.setBounds(50, 316, 300, 31);
-		add(aforo);
 		aforo.setForeground(Color.WHITE);
 		aforo.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		aforo.setBounds(50, 306, 300, 31);
+		contentPane.add(aforo);
 		
-		JLabel dimPant = new JLabel("Dimensiones pantalla:");
-		dimPant.setBounds(50, 366, 300, 31);
-		add(dimPant);
-		dimPant.setForeground(Color.WHITE);
-		dimPant.setFont(new Font("Tahoma", Font.PLAIN, 25));	
+		JLabel dimPantalla = new JLabel("Dimensiones pantalla:");
+		dimPantalla.setForeground(Color.WHITE);
+		dimPantalla.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		dimPantalla.setBounds(50, 356, 300, 31);
+		contentPane.add(dimPantalla);
 		
-		JCheckBox discapacitados = new JCheckBox("Accesible discapacitados: ");
-		discapacitados.setBounds(50, 416, 325, 31);
-		discapacitados.setForeground(Color.WHITE);
-		discapacitados.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		add(discapacitados);
+		JCheckBox accDiscapac = new JCheckBox("Accesible discapacitados: ");
+		accDiscapac.setForeground(Color.WHITE);
+		accDiscapac.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		accDiscapac.setBounds(50, 406, 325, 31);
+		contentPane.add(accDiscapac);
 		
-		JLabel anyoInaguracion = new JLabel("A\u00F1o inauguraci\u00F3n:");
-		anyoInaguracion.setBounds(50, 466, 300, 31);
-		add(anyoInaguracion);
-		anyoInaguracion.setForeground(Color.WHITE);
-		anyoInaguracion.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		JLabel anyoInag = new JLabel("A\u00F1o inauguraci\u00F3n:");
+		anyoInag.setForeground(Color.WHITE);
+		anyoInag.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		anyoInag.setBounds(50, 456, 300, 31);
+		contentPane.add(anyoInag);
 		
 		JLabel responsable = new JLabel("Responsable:");
-		responsable.setBounds(50, 516, 300, 31);
-		add(responsable);
 		responsable.setForeground(Color.WHITE);
 		responsable.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		responsable.setBounds(50, 506, 300, 31);
+		contentPane.add(responsable);
 		
+		JComboBox txtResponsable = new JComboBox();
+		txtResponsable.setBounds(450, 506, 300, 31);
+		contentPane.add(txtResponsable);
 		
+		JButton btnOk = new JButton("");
+		btnOk.setContentAreaFilled(false);
+		btnOk.setBounds(627, 50, 107, 89);
+		contentPane.add(btnOk);
+		btnOk.setIcon(new ImageIcon(VAltaEmpl.class.getResource("/imagenes/BOTONES/botOK.png")));
 		
-		JComboBox txtResponsable_1 = new JComboBox();
-		txtResponsable_1.setBounds(450, 516, 300, 31);
-		add(txtResponsable_1);
+		JButton btnCancelar = new JButton("");
+		btnCancelar.setContentAreaFilled(false);
+		btnCancelar.setBounds(50, 637, 181, 80);
+		contentPane.add(btnCancelar);
+		btnCancelar.setIcon(new ImageIcon(VAltaEmpl.class.getResource("/imagenes/BOTONES/botCANCELAR.png")));
 		
-		JButton ok = new JButton("");
-		ok.setIcon(new ImageIcon(VAltaSalas.class.getResource("/imagenes/BOTONES/botOK.png")));
-		ok.setBounds(659, 50, 91, 80);
-		ok.setContentAreaFilled(false);
-		add(ok);
+		JLabel altaSala = new JLabel("| Alta Sala");
+		altaSala.setForeground(Color.WHITE);
+		altaSala.setFont(new Font("Tahoma", Font.BOLD, 25));
+		altaSala.setBounds(50, 40, 300, 31);
+		contentPane.add(altaSala);
 		
-		JButton cancelar = new JButton("");
-		cancelar.setBounds(50,670,181,80);
-		cancelar.setIcon(new ImageIcon(VAltaSalas.class.getResource("/imagenes/BOTONES/botCANCELAR.png")));
-		cancelar.setContentAreaFilled(false);
-		add(cancelar);
+		JComboBox txtDimPantalla = new JComboBox();
+		txtDimPantalla.setBounds(450, 356, 300, 31);
+		contentPane.add(txtDimPantalla);
 		
-		JLabel label = new JLabel("| Alta Sala");
-		label.setForeground(Color.WHITE);
-		label.setFont(new Font("Tahoma", Font.BOLD, 25));
-		label.setBounds(50, 50, 300, 31);
-		add(label);
-		
-		JComboBox txtDimensiones = new JComboBox();
-		txtDimensiones.setBounds(450, 366, 300, 31);
-		add(txtDimensiones);
-		
-		JYearChooser txtAnyoInagu = new JYearChooser();
-		txtAnyoInagu.setBounds(450, 466, 300, 31);
-		add(txtAnyoInagu);
+		JYearChooser txtAnyoInag = new JYearChooser();
+		txtAnyoInag.setBounds(450, 456, 300, 31);
+		contentPane.add(txtAnyoInag);
 		
 		JSpinner txtNumSala = new JSpinner();
-		txtNumSala.setBounds(450, 275, 300, 31);
-		add(txtNumSala);
+		txtNumSala.setBounds(450, 265, 300, 31);
+		contentPane.add(txtNumSala);
 		
 		JSpinner txtAforo = new JSpinner();
-		txtAforo.setBounds(450, 321, 300, 31);
-		add(txtAforo);
+		txtAforo.setBounds(450, 311, 300, 31);
+		contentPane.add(txtAforo);
 		
-	
+		setLocationRelativeTo(null);
 	}
 }
