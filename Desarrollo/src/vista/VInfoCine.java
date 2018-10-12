f00f7ffbb84374d27b8a95e31326fb39db3f1316
Sheilapsm <murgia85@gmail.com>
2018-10-12 17:23:30 +0200

    validaciones

--- Desarrollo/src/vista/VInfoCine.java
+++ Desarrollo/src/vista/VInfoCine.java
@@ -1,6 +1,7 @@
 package vista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Rectangle;
@@ -9,6 +10,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class VInfoCine extends JFrame {
 
@@ -47,8 +51,41 @@ public class VInfoCine extends JFrame {
 		setBounds(0, 0, 800, 800);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		getContentPane().setBackground(new Color(233, 69, 75));
+		contentPane.setLayout(null);
+		
+		JButton btnVerSalas = new JButton("");
+		btnVerSalas.setBounds(50, 225, 86, 86);
+		contentPane.add(btnVerSalas);
+		
+		JButton btnListadoEmpleados = new JButton("");
+		btnListadoEmpleados.setBounds(50, 64, 86, 86);
+		contentPane.add(btnListadoEmpleados);
+		
+		JLabel lblCine = new JLabel("cine");
+		lblCine.setForeground(Color.WHITE);
+		lblCine.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblCine.setBounds(208, 69, 127, 25);
+		contentPane.add(lblCine);
+		
+		JLabel lblDireccin = new JLabel("Direcci\u00F3n:");
+		lblDireccin.setForeground(Color.WHITE);
+		lblDireccin.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblDireccin.setBounds(208, 134, 127, 25);
+		contentPane.add(lblDireccin);
+		
+		JLabel lblFechaFundacin = new JLabel("Fecha fundaci\u00F3n: ");
+		lblFechaFundacin.setForeground(Color.WHITE);
+		lblFechaFundacin.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblFechaFundacin.setBounds(208, 206, 199, 25);
+		contentPane.add(lblFechaFundacin);
+		
+		JLabel lblTelfono = new JLabel("Tel\u00E9fono: ");
+		lblTelfono.setForeground(Color.WHITE);
+		lblTelfono.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblTelfono.setBounds(208, 276, 127, 25);
+		contentPane.add(lblTelfono);
 	}
-
 }
