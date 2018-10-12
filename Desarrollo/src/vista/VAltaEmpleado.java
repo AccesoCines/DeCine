81ca6d4e1f88e12f7df7761d9cf180657390823b
Sheilapsm <murgia85@gmail.com>
2018-10-12 17:23:10 +0200

    validaciones

--- Desarrollo/src/vista/VAltaEmpleado.java
+++ Desarrollo/src/vista/VAltaEmpleado.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -14,15 +17,21 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import com.toedter.calendar.JDateChooser;
+
+import modelo.Cargo;
+import modelo.Empleado;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 
 public class VAltaEmpleado extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField txtnombre;
+	private JTextField txtapellido;
+	private JTextField txtnaci;
 
 	/**
 	 * Launch the application.
@@ -61,48 +70,104 @@ public class VAltaEmpleado extends JFrame {
 		getContentPane().setBackground(new Color(233, 69, 75));
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(333, 157, 205, 22);
-		contentPane.add(textField);
+		txtnombre = new JTextField();
+		txtnombre.setColumns(10);
+		txtnombre.setBounds(333, 157, 205, 22);
+		contentPane.add(txtnombre);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(334, 213, 204, 22);
-		contentPane.add(textField_1);
+		txtapellido = new JTextField();
+		txtapellido.setColumns(10);
+		txtapellido.setBounds(334, 213, 204, 22);
+		contentPane.add(txtapellido);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(333, 266, 205, 22);
-		contentPane.add(comboBox);
+		JComboBox txtcargo = new JComboBox();
+		txtcargo.setBounds(333, 266, 205, 22);
+		contentPane.add(txtcargo);
 		
-		JButton button = new JButton("");
-		button.setContentAreaFilled(false);
-		button.setBounds(12, 568, 244, 107);
-		contentPane.add(button);
-		button.setIcon(new ImageIcon(VAltaEmpl.class.getResource("/imagenes/BOTONES/botCANCELAR.png")));
+	
+		JButton cancelar = new JButton("");
+		cancelar.setIcon(new ImageIcon(VAltaEmpl.class.getResource("/imagenes/BOTONES/botCANCELAR.png")));
+		cancelar.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				//TOODO FALTA PROBAR CUANDO EST� LA VENTANA DE LISTADOS
+				((java.awt.Window) getParent()).dispose();
+				VListado list = new VListado();
+				list.setVisible(true);
+				}
+		});
 		
-		JButton button_1 = new JButton("");
-		button_1.setContentAreaFilled(false);
-		button_1.setBounds(594, 13, 126, 99);
-		contentPane.add(button_1);
-		button_1.setIcon(new ImageIcon(VAltaEmpl.class.getResource("/imagenes/BOTONES/botOK.png")));
+		cancelar.setBounds(37, 599, 244, 107);
+		contentPane.add(cancelar);
+		cancelar.setContentAreaFilled(false);
 		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(333, 326, 205, 22);
-		contentPane.add(dateChooser);
+		JButton ok = new JButton("");
+		ok.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if (txtnombre.getText().toCharArray().length<2 ) {
+					//validar campo +2 letras
+					JOptionPane.showMessageDialog(null, "No admite menos de dos letras..", "Error en nombre", JOptionPane.ERROR_MESSAGE);
+				}
+				if (txtapellido.getText().toCharArray().length<2) {
+					JOptionPane.showMessageDialog(null, "No admite menos de dos letras..", "Error en apellido", JOptionPane.ERROR_MESSAGE);
+					
+				}
+				if (txtnaci.getText().toCharArray().length<2) {
+					JOptionPane.showMessageDialog(null, "No admite menos de dos letras..", "Error en nacionalidad", JOptionPane.ERROR_MESSAGE);
+				}
+				Cargo car = Cargo.camarero;
+
+			switch(txtcargo.getSelectedItem().toString()){
+				case "camarero":
+					car = Cargo.camarero;
+					break;
+				case "portero":
+					car = Cargo.portero;
+					break;
+				case "acomodadorResponsableBar":
+					car = Cargo.acomodadorResponsableBar;
+					break;
+				case "mantenimiento":
+					car = Cargo.mantenimiento;
+					break;
+				case "responsableCine":
+					car = Cargo.responsableCine;
+					break;
+				case "responsableSala":
+					car = Cargo.responsableSala;
+				break;
+				}
+				
+				Empleado emple = new Empleado(txtnombre.getText(), txtapellido.getText(), car, txtfecCont., (Date)txtfecNac.getDate(), txtnaci.getText(), (Date)txtfecFinCon.getDate(), true);
+	
+				emple.guardarEmpleado();
+				
+				//TODO: PROBAR SI FUNSIONA
+			}
+		});
 		
-		JDateChooser dateChooser_1 = new JDateChooser();
-		dateChooser_1.setBounds(333, 378, 205, 22);
-		contentPane.add(dateChooser_1);
+		ok.setIcon(new ImageIcon(VAltaEmpl.class.getResource("/imagenes/BOTONES/botOK.png")));
+		ok.setBounds(619, 44, 126, 99);
+		contentPane.add(ok);
+		ok.setContentAreaFilled(false);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(333, 427, 205, 22);
-		contentPane.add(textField_2);
+		JDateChooser txtfecCont = new JDateChooser();
+		txtfecCont.setBounds(333, 326, 205, 22);
+		contentPane.add(txtfecCont);
 		
-		JDateChooser dateChooser_2 = new JDateChooser();
-		dateChooser_2.setBounds(333, 485, 203, 22);
-		contentPane.add(dateChooser_2);
+		JDateChooser txtfecNac = new JDateChooser();
+		txtfecNac.setBounds(333, 378, 205, 22);
+		contentPane.add(txtfecNac);
+		
+		txtnaci = new JTextField();
+		txtnaci.setColumns(10);
+		txtnaci.setBounds(333, 427, 205, 22);
+		contentPane.add(txtnaci);
+		
+		JDateChooser txtfecFinCon = new JDateChooser();
+		txtfecFinCon.setBounds(333, 485, 203, 22);
+		contentPane.add(txtfecFinCon);
 		
 		JLabel label = new JLabel("Nombre:");
 		label.setForeground(Color.WHITE);
